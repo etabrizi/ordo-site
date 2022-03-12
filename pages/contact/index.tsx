293ccb6470cd8b04d519737f923b913a8c09e30d
Emyr Tabrizi <etabrizi1@gmail.com>
2022-03-12 15:14:52 +0000

    form work

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -27,21 +27,20 @@ const Contact = () => {
             </a>
           </p>
           <p>Or send us a message</p>
-          <form name="contact" data-netlify="true">
-            <p>
-              <label>
-                Name <input type="text" name="name" />
-              </label>
-            </p>
-            <p>
-              <label>
-                Email <input type="email" name="email" />
-              </label>
-            </p>
-            <p>
-              <button type="submit">Send</button>
-            </p>
-          </form>
+          <form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
           <ul className="links">
             <li>
               <a

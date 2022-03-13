7c7885805f693171222b26f2f1ab97bd2db315cc
Emyr Tabrizi <etabrizi1@gmail.com>
2022-03-13 12:33:53 +0000

    form updates

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,11 +1,40 @@
 import Head from "next/head";
 import Layout from "../../components/Layout";
 import ContentBlock from "../../components/ContentBlock";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ThemeContext } from "../../contexts/themeContext";
 
 const Contact = () => {
   const { lightTheme } = useContext(ThemeContext);
+  
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [formDisabled, setFormDisabled] = useState(true);
+ 
+  const setNameForm = (event : any) => {
+    setName(event.target.value)
+    checkForm()
+  }
+
+  const setEmailForm = (event : any) => {
+    setEmail(event.target.value)
+    checkForm()
+  }
+
+  const setMessageForm = (event : any) => {    
+    setMessage(event.target.value)
+    checkForm()
+  }
+
+  const checkForm = () => {
+    if((name !== '') && (email !== '') && (message !== '')) {
+      setFormDisabled(false);
+    }else{
+      setFormDisabled(true);
+    }
+  }
+
   return (
     <>
       <Head>
@@ -13,39 +42,39 @@ const Contact = () => {
           Ordo Consultancy Ltd - A Digital Consultancy Service - Contact us
         </title>
       </Head>
-      <Layout pageTitle="Let's talk" activeNav="contact">
+      <Layout pageTitle="Let's connect" activeNav="contact">
         <ContentBlock>
           <h3>Contact</h3>
           <p>
-            In the age of automation and messaging, just a friendly chat is a
-            great way for introduction. Or alternatively, contact us via our
-            social platforms.
+            If you're intrested in using our services, please use our enquiry
+            form. Alternatively, contact us via our social media platforms.
           </p>
-          <p>
-            <a className="telephone" href="tel:44 7850 174 596">
-              (m)+44 7850 174 596
-            </a>
-          </p>
-          <p>Or send us a message</p>
-          <form name="contact" action="/success" method="POST" data-netlify="true">
-          <input type="hidden" name="form-name" value="contact" />
+          <form
+            name="contact"
+            action="/success"
+            method="POST"
+            data-netlify="true"
+          >
+            <input type="hidden" name="form-name" value="contact" />
             <p>
               <label>
-                Your Name: <input type="text" name="name" />
+                Your Name: <input onChange={setNameForm} type="text" name="name" />
               </label>
             </p>
             <p>
               <label>
-                Your Email: <input type="email" name="email" />
+                Your Email: <input type="email" onChange={setEmailForm} name="email" />
               </label>
             </p>
             <p>
               <label>
-                Message: <textarea name="message"></textarea>
+                Message: <textarea name="message" onChange={setMessageForm}></textarea>
               </label>
             </p>
             <p>
-              <button type="submit">Send</button>
+              <button disabled={formDisabled} className="button" type="submit">
+                Send
+              </button>
             </p>
           </form>
           <ul className="links">
@@ -90,6 +119,40 @@ const Contact = () => {
             transition: color 0.2s;
           }
 
+          form {
+            width: 100%;
+          }
+
+          form p {
+            width: 100%;
+          }
+
+          form label {
+            width: 100%;
+            display: block;
+          }
+
+          form input,
+          form textarea {
+            width: 100%;
+            padding: 10px;
+            border: 1px solid grey;
+            font-family: arial;
+          }
+
+          form textarea {
+            min-height: 150px;
+          }
+
+          .hide {
+            display: none;
+          }
+
+          .warning {
+            padding: 10px;
+            border: 1px solid #fd9c03;
+          }
+
           .links {
             list-style: none;
             padding: 0;
@@ -119,6 +182,32 @@ const Contact = () => {
           .contact-links span:last-child {
             margin-top: 7px;
             margin-left: 10px;
+          }
+
+          .button {
+            font-size: 1.4em;
+            text-align: center;
+            background: #ffa800;
+            bottom: 0;
+            display: block;
+            padding: 7px 20px;
+            border-radius: 1.6em;
+            text-decoration: none;
+            border: none;
+            color: #000;
+            transition: scale 12.2s;
+            margin-bottom: 30px;
+            cursor: pointer;
+          }
+          .button:hover {
+            background: #e0e0e0;
+            transform: scale(1.05, 1.05);
+            transition: transform 0.2s;
+          }
+
+          .button:disabled {
+            opacity: 0.6;
+            background: #b9b9b9;
           }
         `}</style>
       </Layout>

2e8205950fd56fb0c5af85da4868093fb41acfc0
etabrizi <etabrizi1@gmail.com>
2024-08-20 09:36:58 +0100

    added chat

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,23 +1,39 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-    static async getInitialProps(ctx: any) {
-        const initialProps = await Document.getInitialProps(ctx)
-        return { ...initialProps }
+    static async getInitialProps(ctx : any) {
+        const initialProps = await Document.getInitialProps(ctx);
+        return { ...initialProps };
     }
 
     render() {
         return (
             <Html lang="en">
-                <Head />
+                <Head>
+                    {/* Other head elements can go here */}
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
+                    {/* Tawk.to Script */}
+                    <script type="text/javascript">
+                        {`
+                            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+                            (function(){
+                                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                                s1.async=true;
+                                s1.src='https://embed.tawk.to/66c45425ea492f34bc080a77/1i5nehd39';
+                                s1.charset='UTF-8';
+                                s1.setAttribute('crossorigin','*');
+                                s0.parentNode.insertBefore(s1,s0);
+                            })();
+                        `}
+                    </script>
+                    {/* End of Tawk.to Script */}
                 </body>
             </Html>
-        )
+        );
     }
 }
 
-export default MyDocument
+export default MyDocument;

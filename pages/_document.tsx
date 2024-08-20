fd28e8c12cd6cd4ddf6d69f455647aa06043d5f6
etabrizi <etabrizi1@gmail.com>
2024-08-20 09:41:30 +0100

    chat fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-    static async getInitialProps(ctx : any) {
+    static async getInitialProps(ctx) {
         const initialProps = await Document.getInitialProps(ctx);
         return { ...initialProps };
     }
@@ -16,19 +16,22 @@ class MyDocument extends Document {
                     <Main />
                     <NextScript />
                     {/* Tawk.to Script */}
-                    <script type="text/javascript">
-                        {`
-                            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-                            (function(){
-                                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-                                s1.async=true;
-                                s1.src='https://embed.tawk.to/66c45425ea492f34bc080a77/1i5nehd39';
-                                s1.charset='UTF-8';
-                                s1.setAttribute('crossorigin','*');
-                                s0.parentNode.insertBefore(s1,s0);
-                            })();
-                        `}
-                    </script>
+                    <script
+                        type="text/javascript"
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+                                (function(){
+                                    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                                    s1.async=true;
+                                    s1.src='https://embed.tawk.to/66c45425ea492f34bc080a77/1i5nehd39';
+                                    s1.charset='UTF-8';
+                                    s1.setAttribute('crossorigin','anonymous');
+                                    s0.parentNode.insertBefore(s1,s0);
+                                })();
+                            `,
+                        }}
+                    />
                     {/* End of Tawk.to Script */}
                 </body>
             </Html>
